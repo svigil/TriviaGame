1354db54d4e7d4b198eb81ba4318c9ab25dd94f1
Spencer Vigil <spencervigil27@gmail.com>
2018-08-03 22:48:08 -0500

    getting there

--- assets/java/trivia.js
+++ assets/java/trivia.js
@@ -25,8 +25,38 @@ function check (){
             correct++;
         }
 
+
+
+    var messages = ["100 points for Griffindor!", "Meh", "You're expelled from Hogwarts" ];
+    var pictures = ["assets/images/wizardharry.gif", "assets/images/herm.gif", "assets/images/vold.gif" ];
+    var range = 0;
+
+    if (correct < 1 ){
+        range = 2;
+    }
+    if (correct > 0 && correct < 4 ){
+        range = 1;
+    }
+    if (correct > 3 ){
+        range = 0;
+    }
+    
+    
+    // console.log(range);
+
+    console.log(messages[range]);
+
     document.getElementById("afterSubmit").style.visibility = "visible";
-    document.getElementById("numberCorrect").innerHTML ="You got " + correct + " correct.";
-    console.log(correct);
+
+    document.getElementById("message").innerHTML = messages[range];
+
+    document.getElementById("numberCorrect").innerHTML = "You got " + correct + " correct.";
+
+    document.getElementById("picture").src = pictures[range];;
+
+   
+    // console.log(correct);
+    // console.log(range);
+    
 
 }
